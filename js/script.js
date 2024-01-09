83b218324710d32a9d64a4b9318310f9d7454f53
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-09 14:23:39 +0100

    Created Objects Array

--- js/script.js
+++ js/script.js
@@ -5,7 +5,37 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Template HTML CSS Vue',
+            message: 'To Do List',
+
+            todos: [
+                {
+                    text : 'Completare la Milestone 1',
+                    done: true 
+                }, 
+                {
+                    text : 'Completare la Milestone 2',
+                    done: false 
+                }, 
+                {
+                    text : 'Completare la Milestone 3',
+                    done: false 
+                }, 
+                {
+                    text : 'Finire l\'esercizio di oggi',
+                    done: false 
+                }, 
+                {
+                    text : 'Compleatre il 1^ Bonus',
+                    done: false, 
+                },
+                {
+                    text : 'Compleatre il 2^ Bonus',
+                    done: false, 
+                }
+
+
+            ]
+
         };
     }
   // Monto l'istanza di Vue in pagina
